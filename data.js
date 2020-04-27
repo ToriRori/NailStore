9e541d69e28562bff768ecef4be8e7fda9020355
ToriRori <torirorik@gmail.com>
2020-04-28 01:00:04 +1100

    Add data models

---
+++ data.js
@@ -0,0 +1,43 @@
+module.exports = function () {
+  return {
+    products: [
+      {
+        id: 1, name: "Grattol Opal", category: "Nail Polish",
+        description: "Golden-orange with little shine", price: 365
+      },
+      {
+        id: 2, name: "Essie 61", category: "Nail Polish",
+        description: "Russian roulette", price: 387
+      },
+      {
+        id: 3, name: "OPI Big Apple Red", category: "Nail Polish",
+        description: "Vibrant red", price: 480
+      },
+      {
+        id: 4, name: "Blazek Glass Nail File", category: "Nail File",
+        description: "Czech glass ", price: 349
+      },
+      {
+        id: 5, name: "Opi Edge White Nail File 240", category: "Nail File",
+        description: "With diamond chips", price: 290
+      },
+      {
+        id: 6, name: "JessNail JD4500", category: "Nail Drill",
+        description: "30000 rpm", price: 2790
+      },
+      {
+        id: 7, name: "InGarden Artificial Nail Remover", category: "Nail Remover",
+        description: "For acrylic and gel nails", price: 1160
+      },
+      {
+        id: 8, name: "Kodi Rubber Base", category: "Base Gel",
+        description: "Crystal clear", price: 600
+      },
+      {
+        id: 9, name: "Grattol No Wipe Top Gel", category: "Top Gel",
+        description: "Good consistent", price: 435
+      }
+    ],
+    orders: []
+  }
+}
